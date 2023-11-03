671f0724f5ade5e7189d8a97ed735a606ebc619f
muafalah <muh.ifa.amrillah@gmail.com>
2023-11-03 23:08:10 +0700

    add footer section & scrollup button

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,36 @@ let swiperProduct = new Swiper(".product__swiper", {
     },
   },
 });
+
+// Show Scroll Up
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // scroll more than 350 viewport
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+// Scroll Section Active Link
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
